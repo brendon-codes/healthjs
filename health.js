b25a79da2ea57a7d56f12fafac48fc50f74d1d88
Brendon Crawford <brendon@aphexcreations.net>
2010-11-08 02:27:47 -0800

    * Updated README

--- health.js
+++ health.js
@@ -44,29 +44,29 @@ App.prevIdle = {};
 App.main = function(args) {
     var port, host;
     if (args[2] === '--help') {
-	console.log('Usage: health.js [IP, [PORT]]');
-	return 1;
+        console.log('Usage: health.js [IP, [PORT]]');
+        return 0;
     }
     else {
-	host = args[2];
-	port = args[3];
+        host = args[2];
+        port = args[3];
     }
     if (host === undefined) {
-	host = '127.0.0.1';
+        host = '127.0.0.1';
     }
-    else if (!net.isIP(host)) {
-	console.log('You must supply a valid IP.');
-	return 1;
-    }
+    //else if (!net.isIP(host)) {
+    //  console.log('You must supply a valid IP.');
+    //  return 1;
+    //}
     if (port === undefined) {
-	port = 37778;
+       port = 37778;
     }
     else if (isNaN(port)) {
-	console.log('Port is invalid.');
-	return 1;
+        console.log('Port is invalid.');
+        return 1;
     }
     else {
-	port = parseInt(port);
+        port = parseInt(port);
     }
     console.log("Listening on", host, "port", port);
     net.createServer(App.connected).listen(port, host);
@@ -83,16 +83,16 @@ App.connected = function (socket) {
     var timer;
     socket.setEncoding("utf8");
     socket.on('data', function(msg) {
-	var command;
-	command = App.getCommand(msg);
-	socket.removeAllListeners('data');
-	if (command === 1) {
-	    App.process(socket, true);
-	}
-	else if (command === 2) {
-	    App.loop(socket);
-	}
-	return true;
+        var command;
+        command = App.getCommand(msg);
+        socket.removeAllListeners('data');
+        if (command === 1) {
+            App.process(socket, true);
+        }
+        else if (command === 2) {
+            App.loop(socket);
+        }
+        return true;
     });
     return true;
 };
@@ -108,20 +108,20 @@ App.getCommand = function (msg) {
     p = msg.replace(/^\s+|\s+$/g, '').split(/\s+/g);
     out = 0;
     if (p.length === 3) {
-	if (p[0].toLowerCase() === 'get') {
-	    if (p[1].toLowerCase() === 'cpu') {
-		t = p[2];
-		if (t !== undefined) {
-		    t = t.toLowerCase();
-		    if (t === 'once') {
-			out = 1;
-		    }
-		    else if (t === 'loop') {
-			out = 2;
-		    }
-		}
-	    }
-	}
+        if (p[0].toLowerCase() === 'get') {
+            if (p[1].toLowerCase() === 'cpu') {
+                t = p[2];
+                if (t !== undefined) {
+                    t = t.toLowerCase();
+                    if (t === 'once') {
+                        out = 1;
+                    }
+                    else if (t === 'loop') {
+                        out = 2;
+                    }
+                }
+            }
+        }
     }
     return out;
 };
@@ -135,14 +135,14 @@ App.getCommand = function (msg) {
 App.loop = function(socket) {
     var timer;
     timer = setInterval(function() {
-	if (socket.readyState === 'open') {
-	    App.process(socket, false);
-	    return true;
-	}
-	else {
-	    clearInterval(timer);
-	    socket.end();
-	    return false;
+        if (socket.readyState === 'open') {
+            App.process(socket, false);
+            return true;
+        }
+        else {
+            clearInterval(timer);
+            socket.end();
+            return false;
         }
     }, 2000);
     return true;
@@ -159,16 +159,16 @@ App.process = function (socket, closeIt) {
     var r, fData;
     fData = '';
     r = fs.createReadStream('/proc/stat', {
-	'flags' : 'r',
-	'encoding' : 'ascii'
+        'flags' : 'r',
+        'encoding' : 'ascii'
     });
     r.addListener('data', function(data) {
-	fData += data;
-	return true;
+        fData += data;
+        return true;
     });
     r.addListener('end', function() {
-	App.gotStats(socket, fData, closeIt);
-	return true;
+        App.gotStats(socket, fData, closeIt);
+        return true;
     });
     return true;
 };
@@ -187,7 +187,7 @@ App.gotStats = function(socket, data, closeIt) {
     o = App.output(allStats);
     socket.write(o);
     if (closeIt) {
-	socket.end();
+        socket.end();
     }
     return true;
 };
@@ -202,11 +202,11 @@ App.output = function (allStats) {
     var o;
     o = "";
     for (i = 0, ii = allStats.length; i < ii; i++) {
-	s = allStats[i];
-	o += s[1].toFixed(2);
-	if (i < (ii - 1)) {
-	    o += " ";
-	}
+        s = allStats[i];
+        o += s[1].toFixed(2);
+        if (i < (ii - 1)) {
+            o += " ";
+        }
     }
     o += "\n";
     return o;
@@ -223,61 +223,73 @@ App.build = function(data) {
     d = data.split("\n");
     allStats = [];
     for (i = 0, ii = d.length; i < ii; i++) {
-	s = d[i];
-	if (s.substr(0, 3) === 'cpu') {
-	    fields = s.split(/\s+/g);
-	    cpu = fields.splice(0, 1)[0];
-	    cpuIdent = cpu.charAt(3);
-	    cpuIndex = null;
-	    if (cpuIdent === '') {
-		cpuIndex = -1;
-	    }
-	    else if (!isNaN(cpuIdent)) {
-		cpuIndex = parseInt(cpuIdent);
-	    }
-            //console.log(cpuIndex);
-	    if (cpuIndex !== null) {
-		thisStat = App.getStatRow(cpuIndex, fields);
-		allStats.push([cpuIndex, thisStat]);
-	    }
-	}
+        s = d[i];
+        if (s.substr(0, 3) === 'cpu') {
+            fields = s.split(/\s+/g);
+            cpu = fields.splice(0, 1)[0];
+            cpuIdent = cpu.charAt(3);
+            cpuIndex = null;
+            if (cpuIdent === '') {
+                cpuIndex = -1;
+            }
+            else if (!isNaN(cpuIdent)) {
+                cpuIndex = parseInt(cpuIdent);
+            }
+            if (cpuIndex !== null) {
+                thisStat = App.getStatRow(cpuIndex, fields);
+                allStats.push([cpuIndex, thisStat]);
+            }
+        }
         else {
-	    break;
+            break;
         }
     }
     return allStats;
 };
 
+/**
+ * Get a single stat item
+ *
+ * @param {Int} cpuIndex
+ * @param {Array} fields
+ * @return {Float}
+ */
 App.getStatRow = function(cpuIndex, fields) {
     var calc, diffUsage;
-    //console.log(App.prevTotal[cpuIndex]);
     if (App.prevTotal[cpuIndex] === undefined) {
-	App.prevTotal[cpuIndex] = 0;
+        App.prevTotal[cpuIndex] = 0;
     }
     if (App.prevIdle[cpuIndex] === undefined) {
-	App.prevIdle[cpuIndex] = 0;
+        App.prevIdle[cpuIndex] = 0;
     }
-    //console.log(cpuIndex);
     calc = App.calculate(cpuIndex, fields,
-			 App.prevIdle[cpuIndex],
-			 App.prevTotal[cpuIndex]);
-    //console.log(calc);
+                         App.prevIdle[cpuIndex],
+                         App.prevTotal[cpuIndex]);
     diffUsage = calc[0];
     App.prevIdle[cpuIndex] = calc[1];
     App.prevTotal[cpuIndex] = calc[2];
     return diffUsage;
 };
 
+/**
+ * Perform stat calculations
+ * 
+ * @param {Int} cpuIndex
+ * @param {Array} fields
+ * @param {Int} prevIdle
+ * @param {Int} prevTotal
+ * @return {Array}
+ */
 App.calculate = function(cpuIndex, fields, prevIdle, prevTotal) {
     var idle, total, diffIdle, prevTotal;
     idle = fields[3];
     total = fields.reduce(function (p, c) {
-	return (parseInt(p) + parseInt(c));
+       return (parseInt(p) + parseInt(c));
     });
     diffIdle = idle - prevIdle;
     diffTotal = total - prevTotal;
     if (diffTotal === 0) {
-	diffUsage = 0.0;
+       diffUsage = 0.0;
     }
     else {
         diffUsage = (((diffTotal - diffIdle) / diffTotal) * 100);
@@ -286,4 +298,9 @@ App.calculate = function(cpuIndex, fields, prevIdle, prevTotal) {
     return out;
 };
 
-App.main(process.argv);
+var ret = App.main(process.argv);
+if (ret > 0) {
+    process.exit(ret);
+}
+
+
